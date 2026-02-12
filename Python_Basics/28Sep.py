561905ea6b0ab323f098c61054dfbe5cf93a270c
abhinav84094 <abhinav84094@gmail.com>
2026-02-12 15:50:51 +0530

    Python Basics Practice file uploaded

---
+++ Python_Basics/28Sep.py
@@ -0,0 +1,20 @@
+# length = input("enter length: ")
+# breadth = input("enter breadth: ")
+
+# area = int(length) * int(breadth)
+
+
+# area = 3.14 * float(length) * float(length)
+
+# perimeter = 2 * 3.14 * float(length)
+
+
+# ans = area / perimeter
+
+# print(ans)
+# print("ans : ", ans)
+# print("ans : {}".format(ans))
+# print(f"ans : {ans}")
+
+
+
